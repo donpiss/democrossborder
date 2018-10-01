6f1de3a1a06b55667d66640bf65ac15ab966ebf2
donpiss <donn_etral@yahoo.com>
2018-10-01 14:24:30 +0700

    first commit

---
+++ src/main/java/com/transkargo/democrossborder/DemocrossborderApplication.java
@@ -0,0 +1,12 @@
+package com.transkargo.democrossborder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocrossborderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocrossborderApplication.class, args);
+	}
+}
